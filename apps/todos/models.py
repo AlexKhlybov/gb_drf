9b66ae0ba048607a805b5beef72a6bd916881c56
Хлыбов А.Г <AGKhlybov@sberbank.ru>
2022-03-08 17:25:19 +0300

    Добавлена модель - Проект

--- apps/todos/models.py
+++ apps/todos/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from uuid import uuid4
 
-# Create your models here.
+
+class Project(models.Model):
+    
+    id = models.UUIDField(primary_key=True, default=uuid4)
+    name = models.CharField(max_length=128, verbose_name='Название')
+    repo = models.URLField(verbose_name='Репозиторий')
+    user = models.ManyToManyField('User', db_index=True, blank=True, verbose_name='Пользователи')
+    
+    class Meta:
+        verbose_name = "Проект"
+        verbose_name_plural = "Проекты"
+    
+    def __str__(self):
+        return f"{self.name}"
+
